0f6d801730ce31017bc92dd59d5dfae99d2ab750
Javarris Barnett <javarrisb@gmail.con>
2022-01-06 23:47:53 +0100

    Create GET routes

--- server.js
+++ server.js
@@ -21,20 +21,43 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
-/*db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
- });
+// GET all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
  
 
  // GET a single candidate
- db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-     if (err) {
-         console.log(err);
-     }
-     console.log(row);
+ app.get('/api/candidates/:id', (req, res) => {
+     const sql = `SELECT * FROM candidates WHERE id = ?`;
+     const params = [req.params.id];
+
+     db.query(sql, params, (err, row) => {
+         if (err) {
+             res.status(400).json({ error: err.message });
+             return;
+         }
+         res.json({
+             message: 'success',
+             data: row 
+         });
+     });
  });
  
 
+ /*
 // Delete a candidate
 db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
     if (err) {
@@ -42,7 +65,7 @@ db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
     }
     console.log(result);
 });
-*/
+
 
 // Create a candidate
 const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
@@ -59,8 +82,7 @@ db.query(sql, params, (err, result) => {
 app.use((req, res) => {
     res.status(404).end();
 });
-
-
+*/
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
